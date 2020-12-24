e6dc6fe9a3d2d57dbeaca47b34bdf1d44ccfb02d
Duli97 <dwindula@gmail.com>
2020-12-25 01:16:42 +0530

    no message

--- src/main/java/com/luv2code/springboot/springdatajpacruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/springboot/springdatajpacruddemo/service/EmployeeServiceImpl.java
@@ -7,9 +7,8 @@ import com.luv2code.springboot.springdatajpacruddemo.dao.EmployeeRepository;
 import com.luv2code.springboot.springdatajpacruddemo.entity.Employee;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
